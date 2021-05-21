d8713481e661b41ff62a0242ee23f45f2b8df4d2
test2 <test2@test2.com>
2021-05-21 17:31:38 +0900

    lee DaBang Opening commit

---
+++ src/com/lee/dabang/DaBang.java
@@ -0,0 +1,11 @@
+package com.lee.dabang;
+
+public class DaBang {
+
+	public static void main(String[] args) {
+		TeaType tea = TeaType.보성녹차;
+		System.out.println("lee 다방에 오신걸 환영합니다");
+		System.out.println(TeaType.율무차);
+	}
+
+}
---
+++ src/com/lee/dabang/TeaType.java
@@ -0,0 +1,5 @@
+package com.lee.dabang;
+
+public enum TeaType {
+감잎차, 보성녹차, 율무차
+}

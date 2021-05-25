4a996268ceaabf8258353f5f2360046c1292679c
test2 <test2@test2.com>
2021-05-25 17:55:54 +0900

    다방 메뉴 문자열출력기능추가

--- src/com/lee/dabang/DaBang.java
+++ src/com/lee/dabang/DaBang.java
@@ -5,7 +5,20 @@ public class DaBang {
 	public static void main(String[] args) {
 		TeaType tea = TeaType.보성녹차;
 		System.out.println("lee 다방에 오신걸 환영합니다");
-		System.out.println(TeaType.율무차);
+		TeaType teaTyppes[] = TeaType.values();
+		
+		/**
+		 * 나중에 Enum to list ㅅ사용하여 정리
+		 */
+		String menu = String.join("/",
+				TeaType.감잎차.toString(),
+				TeaType.보성녹차.toString(),
+				TeaType.율무차.toString());
+				
+		
+		System.out.println(menu);
+		string[] menus =  menu.split();
+		return;
 	}
 
 }
